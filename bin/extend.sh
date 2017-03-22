a7ae9908715664d8eec55e080ae9580f362ab4a2
Roland Guelle <roland@guelle.de>
2017-03-22 11:00:34 +0100

    changed sh to bash

--- bin/extend.sh
+++ bin/extend.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # extend non-HiDPI external display on DP* above HiDPI internal display eDP*
 # see also https://wiki.archlinux.org/index.php/HiDPI
 # you may run into https://bugs.freedesktop.org/show_bug.cgi?id=39949
